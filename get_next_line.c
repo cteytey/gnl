2a5eb2d2cb4b26a693f0fa7c771435d8d41cc0a9
jules maison <dehon.jules1@gmail.com>
2025-10-25 14:26:52 +0200

    ajout utils

--- get_next_line.c
+++ get_next_line.c
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: judehon <judehon@student.42.fr>            +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 14:34:51 by judehon           #+#    #+#             */
-/*   Updated: 2025/10/24 17:04:02 by judehon          ###   ########.fr       */
+/*   Updated: 2025/10/25 14:26:21 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*ft_get_line(char *tmp)
+{
+	
+}
 
 char	*get_next_line(int fd)
 {
@@ -20,7 +25,9 @@ char	*get_next_line(int fd)
 	int		size;
 
 	buffer = malloc(BUFFER_SIZE + 1);
-	read(fd, buffer, BUFFER_SIZE);
+	size = read(fd, buffer, BUFFER_SIZE);
+	if (size <= 0)
+		return (NULL);
 	while (*buffer != '\n')
 	{
 		s += malloc(sizeof(char) * (BUFFER_SIZE + 1));
---
+++ get_next_line_utils.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/25 14:13:04 by marvin            #+#    #+#             */
+/*   Updated: 2025/10/25 14:13:04 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	int		i;
+
+	dest = malloc(sizeof(const char) * (ft_strlen(s) + 1));
+	i = 0;
+	if (!dest)
+		return (0);
+	while (s[i])
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
