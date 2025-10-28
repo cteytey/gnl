4c47e959fe3f48b0f9561206dd458490a290bdf0
Jules Dehon <judehon@3F2.42angouleme.fr>
2025-10-28 17:09:15 +0100

    read_join fonctionnel manque a trim la fin

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: judehon <judehon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 14:34:51 by judehon           #+#    #+#             */
-/*   Updated: 2025/10/28 16:40:31 by judehon          ###   ########.fr       */
+/*   Updated: 2025/10/28 17:07:24 by judehon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,32 +23,25 @@ static char	*ft_read_join(int fd)
 	if (!buffer)
 		return (NULL);
 	size = 1;
-	while (!ft_strchr(s, '\n') && size > 0)
+	while (!ft_strchr(buffer, '\n') && size > 0)
 	{
 		size = read(fd, buffer, BUFFER_SIZE);
 		buffer[size] = '\0';
 		if (!s)
 			s = ft_strdup(buffer);
 		else
-			s = ft_strjoin(s, buffer);
+		{
+			tmp = ft_strjoin(s, buffer);
+			free(s);
+			s = tmp;
+		}
 	}
 	free(buffer);
-	return (s);
-}
-
-char	*get_next_line(int fd)
-{
-	char	*s;
-	int		i;
-
-	i = 0;
-	while (s[i] != '\n')
-		s = ft_print_line(fd);
 	return (s);
 }
 
 #include <stdio.h>
 int	main()
 {
-	printf("%s\n", get_next_line(open("get_next_line.c", O_RDWR)));
+	printf("%s\n", ft_read_join(open("get_next_line.c", O_RDWR)));
 }
