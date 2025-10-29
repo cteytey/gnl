d6736233aada31957dcde2359178be13c89a7c63
Jules Dehon <judehon@1G2.42angouleme.fr>
2025-10-29 17:27:38 +0100

    ligne entiere qui s'arrete au \n

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,33 @@
 /*   By: judehon <judehon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 14:34:51 by judehon           #+#    #+#             */
-/*   Updated: 2025/10/28 17:07:24 by judehon          ###   ########.fr       */
+/*   Updated: 2025/10/29 17:27:02 by judehon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_remove_end(char *str)
+{
+	int	i;
+	int	j;
+	int	tmp;
+	char	*new;
+
+	i = ft_strlen(str);
+	while (str[i] != '\n')
+		i--;
+	tmp = i;
+	new = malloc(sizeof(char) * tmp + 1);
+	if (!new)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (j < tmp)
+		new[j++] = str[i++];
+	new[j] = '\0';
+	return (new);
+}
 
 static char	*ft_read_join(int fd)
 {
@@ -37,11 +59,18 @@ static char	*ft_read_join(int fd)
 		}
 	}
 	free(buffer);
+	s = ft_remove_end(s);
 	return (s);
+}
+
+char	*get_next_line(int fd)
+{
+
 }
 
 #include <stdio.h>
 int	main()
 {
-	printf("%s\n", ft_read_join(open("get_next_line.c", O_RDWR)));
+	printf("%s\n", ft_read_join(open("test.txt", O_RDWR)));
+	printf("%s\n", ft_read_join(open("test.txt", O_RDWR)));
 }
